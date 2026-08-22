efa644b2854604c765df6ddde543e69d75d4e11d
psk001s <psk001s@users.noreply.github.com>
2026-08-22 22:04:08 +0800

    Prevent stale moments feed caching

--- assets/generated-moments.js
+++ assets/generated-moments.js
@@ -1,1 +1,20 @@
-window.BLOG_MOMENTS = [];
+window.BLOG_MOMENTS = [
+  {
+    "id": "2026-08-22-140200",
+    "date": "2026-08-22 22:01",
+    "location": "",
+    "images": [
+      "/assets/images/uploads/IMG_3261.jpeg"
+    ],
+    "content": "<p><br>.<br></p>"
+  },
+  {
+    "id": "2026-08-22-134403",
+    "date": "2026-08-22 21:41",
+    "location": "",
+    "images": [
+      "/assets/images/uploads/IMG_1750.jpeg"
+    ],
+    "content": "<p><br>骑行🚴🏻‍♀️<br></p>"
+  }
+];
